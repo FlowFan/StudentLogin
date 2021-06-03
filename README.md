46c13341a2bd14573899c258df241191971bec35
fan1138612367 <fan1138612367@vip.qq.com>
2021-06-03 10:44:12 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# StudentLogin
+编写一个用户登录界面（类似qq），界面中必须要有文本提示信息（TextView），编辑框（EditText），按钮（Button），分别用于显示“计算机学院xx级学生”、“口令”，输入用户名、密码，登录功能。
+![img.png](images/img.png)
---
+++ app/src/main/java/com/example/studentlogin/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.studentlogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.btn_login);
+        textView = findViewById(R.id.textView);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("登录成功");
+            }
+        });
+    }
+}
